171230c4c1a15f28fe54cdd6426646548687d371
Sam Gallagher <sgallagher@cloudbakers.com>
2021-12-16 07:07:53 -0600

    added a change

--- src/web_sam/src/index.js
+++ src/web_sam/src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get("/", (req, res) => {
-  res.send("WEB Container!\n\nThis is dev.");
+  res.send("WEB Container!\n\nThis is dev. Another change!");
 });
 
 app.listen(port, () => {
